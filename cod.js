eb29c3c36ec3789a0716c585929c3e404abbdc30
Gui-Campos1 <160081281+Gui-Campos1@users.noreply.github.com>
2024-07-31 09:43:46 -0300

    Add files via upload

--- cod.js
+++ cod.js
@@ -1,34 +1,36 @@
 function calcularValor() {
     // Obter o valor do input e garantir que seja um número
-    var valorInputado = parseFloat(document.getElementById("valorInputado").value);
+    var salarioInputado = parseFloat(document.getElementById("valorInputado").value);
     var mesesTrabalhados = parseInt(document.getElementById("mesesTrabalhados").value);
 
     // Verificar se o valor e os meses são números e positivos
-    if (isNaN(valorInputado) || valorInputado < 0 || isNaN(mesesTrabalhados) || mesesTrabalhados <= 0 || mesesTrabalhados > 12) {
+    if (isNaN(salarioInputado) || salarioInputado < 0 || isNaN(mesesTrabalhados) || mesesTrabalhados <= 0 || mesesTrabalhados > 12) {
         document.getElementById("Adiantamento").innerHTML = "Por favor, insira valores válidos.";
         return;
     }
 
-    // Definir o valor máximo para a soma
-    var valorMaximoParaSoma = 4500;
+    // Definir o valor máximo para o fixo e o total
+    var valorMaximoFixo = 4500;
+    var valorMaximoTotal = 17000;
 
-    // Calcular o valor a ser usado na soma
-    var valorParaSoma = valorInputado > valorMaximoParaSoma ? valorMaximoParaSoma : valorInputado;
+    // Calcular o valor fixo
+    var valorFixo = salarioInputado > valorMaximoFixo ? valorMaximoFixo : salarioInputado;
 
-    // Calcular o bônus total anual
-    var valorFormula = (valorInputado * 1.7) + valorParaSoma;
+    // Calcular o valor variável
+    var valorVariavel = salarioInputado * 1.7;
 
-    // Calcular 40% do valor da fórmula
-    var valorBônusAnual = valorFormula * 0.40;
-
-    // Limitar o bônus anual a 17.000
-    if (valorBônusAnual > 17000) {
-        valorBônusAnual = 17000;
+    // Calcular o total e garantir que não ultrapasse o máximo permitido
+    var total = valorFixo + valorVariavel;
+    if (total > valorMaximoTotal) {
+        total = valorMaximoTotal;
     }
 
+    // Calcular o adiantamento como 40% do total
+    var adiamento = total * 0.40;
+
     // Calcular o bônus proporcional ao número de meses trabalhados
-    var valorBônusProporcional = valorBônusAnual * (mesesTrabalhados / 12);
+    var adiamentoProporcional = adiamento * (mesesTrabalhados / 12);
 
     // Exibir o resultado formatado como moeda em reais
-    document.getElementById("Adiantamento").innerHTML = "Adiantamento: " + valorBônusProporcional.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    document.getElementById("Adiantamento").innerHTML = "Adiantamento: " + adiamentoProporcional.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
